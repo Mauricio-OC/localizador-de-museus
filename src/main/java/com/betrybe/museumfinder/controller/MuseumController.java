d88f6783c149de28883f0c3600903137988ee130
Mauricio-OC <murisc0@hotmail.com>
2023-10-06 22:36:16 -0300

    req 05

--- src/main/java/com/betrybe/museumfinder/controller/MuseumController.java
+++ src/main/java/com/betrybe/museumfinder/controller/MuseumController.java
@@ -1,10 +1,5 @@
 package com.betrybe.museumfinder.controller;
 
-import static com.betrybe.museumfinder.util.ModelDtoConverter.dtoToModel;
-import static com.betrybe.museumfinder.util.ModelDtoConverter.modelToDto;
-
-import com.betrybe.museumfinder.dto.MuseumCreationDto;
-import com.betrybe.museumfinder.dto.MuseumDto;
 import com.betrybe.museumfinder.model.Museum;
 import com.betrybe.museumfinder.service.MuseumServiceInterface;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,25 +8,24 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Museum controller.
  */
 @RestController
-@RequestMapping("/Museums")
+@RequestMapping("/museums")
 public class MuseumController {
-  MuseumServiceInterface service;
+  private final MuseumServiceInterface service;
 
   @Autowired
   public MuseumController(MuseumServiceInterface service) {
     this.service = service;
   }
 
-  @PostMapping()
-  public ResponseEntity<MuseumDto> createMuseum(@RequestBody MuseumCreationDto museumCreationDto) {
-    Museum museum = service.createMuseum(dtoToModel(museumCreationDto));
-    return ResponseEntity.status(HttpStatus.CREATED).body(modelToDto(museum));
+  @PostMapping
+  public ResponseEntity<Museum> createMuseum(@RequestBody Museum museumCreationDto) {
+    Museum createdMuseum = service.createMuseum(museumCreationDto);
+    return ResponseEntity.status(HttpStatus.CREATED).body(createdMuseum);
   }
 }
--- src/main/java/com/betrybe/museumfinder/service/MuseumService.java
+++ src/main/java/com/betrybe/museumfinder/service/MuseumService.java
@@ -6,7 +6,6 @@ import com.betrybe.museumfinder.exception.MuseumNotFoundException;
 import com.betrybe.museumfinder.model.Coordinate;
 import com.betrybe.museumfinder.model.Museum;
 import com.betrybe.museumfinder.util.CoordinateUtil;
-import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
